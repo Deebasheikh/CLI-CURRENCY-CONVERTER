1a6bdb504199b9cddd5322025e0846f2156dc9bc
Areeba Sheikh <areebasheikh27.as@gmail.com>
2024-04-14 13:49:54 -0700

    created cli -currency-converter

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# CLI-CURRENCY-CONVERTER
-This is a cli currency converter.
+## Online CLI-CURRENCY-CONVERTER
+
+Online CLI Currency Converter is a simple command-line tool that allows you to convert currencies based on pre-defined exchange rates. It supports conversion between 15 different currencies.
+
+## Features
+
+- Convert currencies easily from your command line
+- Supports conversion between 15 different currencies
+- Uses pre-defined exchange rates
+
+## Installation
+
+1. Clone the repository:
+https://github.com/Deebasheikh/CLI-CURRENCY-CONVERTER.git
+2. Install dependencies:
+npm i cli-currency-converter
+
+# Usage
+To use the CLI Currency Converter, follow these steps:
+
+1. Run the command below to start the CLI:
+NPX cli-currency-converter
+
+2. Follow the prompts to select the currency you want to convert from, the currency you want to convert to, and enter the amount.
+
+3. The converter will display the converted amount based on the pre-defined exchange rates.
+
+# Supported Currencies
+The CLI Currency Converter supports conversion between the following 15 currencies:
+ 1.USD (United States Dollar)
+ 2.EUR (EURO)
+ 3.GBP (British Pound Sterling)
+ 4.INR (Indian Rupee)
+ 5.PKR (Pakistani Rupee)
+ 6.CAD (Canadian Dollar)
+ 7.SGD (Singaporean Dollar)
+ 8.JPY (Japanese Yen)
+ 9.CNY (Chinese Yuan)
+ 10.SAR (Saudi Riyal)
+ 11.OMR (Omani Riyal)
+ 12.NZD (New Zealand Dollar)
+ 13.KWD (Kuwaiti Dinar)
+ 14.AED (United Arab Emirates Dirham)
+ 15.YER (Yemini Rial)    
---
+++ index.ts
@@ -0,0 +1,89 @@
+#!/usr/bin/env
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency : any = {
+  USD: {rate :1, symbol:"$" }, //BASE CURRENCY
+  EUR: {rate :0.91, symbol:"€"},
+  GBP: {rate :0.76, symbol: "£"},
+  INR  : {rate :74.57, symbol:"₹"},
+  PKR: {rate :280, symbol:"Rs"},
+  CAD: {rate :1.359049, symbol:"CA$"},
+  SGD: {rate :1.348826, symbol:"S$"},
+  JPY: {rate :151.625636, symbol:"¥"},
+  CNY: {rate :7.233266, symbol:"¥"},
+  SAR: {rate :3.75, symbol:"﷼"},
+  OMR: {rate :0.38, symbol:"﷼"},
+  NZD: {rate :1.66, symbol:"$"},
+  KWD: {rate :3.251815, symbol:"د.ك"},
+  AED: {rate :3.67, symbol:" د.إ"},
+  YER: {rate :250.45, symbol:"﷼"},
+};
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Convert from Currency",
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "INR",
+      "PKR",
+      "CAD",
+      "SGD",
+      "JPY",
+      "CNY",
+      "SAR",
+      "OMR",
+      "NZD",
+      "KWD",
+      "AED",
+      "YER",
+    ],
+  },
+  {
+    name: "to",
+    message: "Convert to Currency",
+    type: "list",
+    choices: [
+      "USD",
+      "EUR",
+      "GBP",
+      "INR",
+      "PKR",
+      "CAD",
+      "SGD",
+      "JPY",
+      "CNY",
+      "SAR",
+      "OMR",
+      "NZD",
+      "KWD",
+      "AED",
+      "YER",
+    ],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount",
+    type: "number",
+  },
+]);
+
+
+let fromAmount = currency[user_answer.from].rate ; //exchange rate
+let toAmount = currency[user_answer.to].rate ; //exchange rate
+let newAmount = parseFloat(user_answer.amount);
+let baseAmount = newAmount / fromAmount; //USD base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(
+  chalk.bgWhite.magenta.bold.italic(
+    `Converted amount: ${parseFloat(convertedAmount.toFixed(2))} ${
+      currency[user_answer.to].symbol
+    }`)
+  );
+// console.log(fromAmount)
+// console.log(toAmount)
+// console.log(amount)
+// console.log(convertedAmount)
